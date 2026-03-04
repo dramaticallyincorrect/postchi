49f1e3f026e1a2850ce6c3ac98604a991efc41be
hamedmonji <hamedmonji30@gmail.com>
2026-03-04 22:29:10 +0100

    use single <> for variables

--- src/lib/http/parser/http.test.ts
+++ src/lib/http/parser/http.test.ts
@@ -1,221 +1,212 @@
-import { test } from 'vitest'
+import { describe, it, test } from 'vitest'
 import { parser } from "./parser.js"
 import { testTree } from "@lezer/generator/test"
 
-test('request line', () => {
-  const defaultValue = `
+describe('request line', () => {
+
+  it('absolute url with all characters', () => {
+    const defaultValue = `
   POST https://testpages.eviltester.com/pages/auth/basic-auth/basic-auth%:-results.html#path?a=b&c=d`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path))`
-  testTree(tree, spec)
+    testTree(tree, spec)
 
-})
+  })
 
-test('request line', () => {
-  const defaultValue = `
-GET https://testpages.eviltester.com/pages/auth/basic-auth/basic-auth-results.html
-
-authorization= basic(sdfs)
-
-
-
-@body`;
-
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
-                RequestLine(Method,Path),
-                Header(Key, Function(FunctionName, Value) ),
-                BodyStart)`
-  // printTree(tree, defaultValue)
-  testTree(tree, spec)
-
-})
-
-test('relative at root', () => {
-  const defaultValue = `
+  it('relative at root', () => {
+    const defaultValue = `
   POST /
   `;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path))`
-  testTree(tree, spec)
+    testTree(tree, spec)
 
-})
+  })
 
-test('request line with variables', () => {
-  const defaultValue = `
-  POST /api/v1/data spaces <<url>>allowed <<second>>in path`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+  it('variables', () => {
+    const defaultValue = `
+  POST /api/v1/data spaces <url>allowed <second>in path`;
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path(Variable, Variable)))`
 
-  testTree(tree, spec)
+    testTree(tree, spec)
+
+  })
 
 })
 
-test('header', () => {
-  const defaultValue = `
-POST <<variable>>/api/v1/data spaces <<url>>allowed <<second>>in path
-hea@der : val@ue
-a`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+describe('headers', () => {
+
+  const requestOf = (rest: string) => `
+POST <variable>/api/v1/data spaces <url>allowed <second>in path
+${rest}`.trim()
+
+  it('key : value', () => {
+    const defaultValue = requestOf(`header : value`);
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path(Variable,Variable,Variable)),
-                Header(Key, Value),
-                Header(Key))`
-  testTree(tree, spec)
-
-})
+                Header(Key, Value))`
+    testTree(tree, spec)
+  })
 
 
-test('header no key', () => {
-  const defaultValue = `
+  it('no key', () => {
+    const defaultValue = `
 POST /
 : value/df`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path),
                 Header(Value))`
-  testTree(tree, spec)
+    testTree(tree, spec)
 
-})
+  })
 
 
-test('header only empty space for key are ignored', () => {
-  const defaultValue = `
+  it('empty space before key are ignored', () => {
+    const defaultValue = `
 POST /
   : value/df`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path),
                 Header(Value))`
-  testTree(tree, spec)
+    testTree(tree, spec)
 
-})
+  })
 
-test('header with slashes in value', () => {
-  const defaultValue = `
+  it('header with slashes in value', () => {
+    const defaultValue = `
 POST /path
 header: application/json`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path)
                 Header(Key, Value))`
-  testTree(tree, spec)
+    testTree(tree, spec)
 
-})
+  })
 
-test('multiple headers', () => {
-  const defaultValue = `
+  it('multiple headers', () => {
+    const defaultValue = `
   POST /api/v1/data
   header1: value1 stil value1`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path,),
                 Header(Key, Value))`
-  testTree(tree, spec)
+    testTree(tree, spec)
 
-})
+  })
 
-test('header with variable as values', () => {
-  const defaultValue = `
+  it('key : variable', () => {
+    const defaultValue = `
   POST /api/v1/data
   header1 one : value1 value2
-  header2: <<variable>>`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+  header2: <variable>`;
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path,),
                 Header(Key, Value),
                 Header(Key, Variable))`
-  testTree(tree, spec)
+    testTree(tree, spec)
 
-})
+  })
 
-test('header with function as values', () => {
-  const defaultValue = `
+  it('key : nested function', () => {
+    const defaultValue = `
   POST /api/v1/data
-  header2:basic()`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+  header2: basic(username, join(password, secret))`;
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path,),
-                Header(Key, Function(FunctionName)))`
+                Header(Key, Function(FunctionName, Value, Function(FunctionName,Value,⚠,Value))))`
 
 
-  testTree(tree, spec)
+    testTree(tree, spec)
 
+  })
 })
 
-test('json body', () => {
-  const defaultValue = `
-  POST /api/v1/data
-  @body 
-  {
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-  }`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+describe('body', () => {
+  const requestOf = (body: string,) => `
+POST /
+
+@body
+${body}`.trim()
+  it('json', () => {
+    const defaultValue = requestOf(`
+{
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}`);
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path),
                 BodyStart, Body(JsonBody))`
 
-  testTree(tree, spec)
+    testTree(tree, spec)
 
-})
+  })
 
-test('form body', () => {
-  const defaultValue = `
+  it('form', () => {
+    const defaultValue = `
   POST /api/v1/data
   @body 
   username = john_doe`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path),
                 BodyStart, Body(FormBody(Key, Value)))`
 
-  testTree(tree, spec)
+    testTree(tree, spec)
 
-})
+  })
 
-test('text body', () => {
-  const defaultValue = `
+  it('text body', () => {
+    const defaultValue = `
   POST /api/v1/data
   @body 
   this is a text body`;
-  const tree = parser.parse(defaultValue)
-  let spec = `Request(
+    const tree = parser.parse(defaultValue)
+    let spec = `Request(
                 RequestLine(
                   Method,
                   Path),
                 BodyStart, Body(TextBody))`
 
-  testTree(tree, spec)
+    testTree(tree, spec)
 
+  })
 })
 
 test('comments', () => {
