fe8ee3256dedcf038cf6e192db0f28548bd7e2cb
hamedmonji <hamedmonji30@gmail.com>
2026-03-28 02:28:06 +0100

    center Send Request Instructions

--- src/components/send-request-shortcut.tsx
+++ src/components/send-request-shortcut.tsx
@@ -1,11 +1,11 @@
 import { Kbd, KbdGroup } from "./ui/kbd";
 
 export const SendRequestInstructions = () => (
-    <div className="flex flex-row place-self-center items-center h-full text-muted-foreground">
+    <div className="flex flex-row items-center h-full text-muted-foreground justify-center">
         <span className="mx-8">Send Request</span>
         <KbdGroup>
             <Kbd className="bg-background-panel">⌘</Kbd>
-            <Kbd data-icon="inline-end" className="translate-x-0.5 bg-background-panel">
+            <Kbd data-icon="inline-end" className="-translate-x-1 bg-background-panel">
                 ⏎
             </Kbd>
         </KbdGroup>
