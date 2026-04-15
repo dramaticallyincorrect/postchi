e1fa54197fabfadf4611fa17a5053e89a4ee48ba
hamedmonji <hamedmonji30@gmail.com>
2026-04-15 18:56:03 +0200

    create pinned file on project creation

--- src/postchi/project/project.ts
+++ src/postchi/project/project.ts
@@ -24,6 +24,7 @@ export async function createProject(path: string, fileStorage: FileStorage = Def
     await createIfNotExists(pathOf(path, environmentsName + envExtension), fileStorage)
     await createIfNotExists(pathOf(path, secretsName + envExtension), fileStorage)
     await createIfNotExists(pathOf(path, postchiDirName, sourcesFileName), fileStorage, JSON.stringify({ sources: [] }, null, 2))
+    await createIfNotExists(pinnedPathForProject(path), fileStorage, JSON.stringify({ sources: [] }, null, 2))
     return {
         name,
         path,
